fda494f4fcefe46cce779f84787d08381a202e87
Luke Smith <luke.smith@digital.hmrc.gov.uk>
2017-01-02 15:50:30 +0000

    renamed dir and file, expanded upon basic calculation

--- main.py
+++ main.py
@@ -1,6 +1,40 @@
-from service.core_functions import *
+import re
+from modules.core_math import *
 
-print(add(7, 5))
-print(subtract(7, 5))
-print(multiply(7, 5))
-print(divide(7, 5))
+print("Console Calculator")
+print("Type 'quit' to exit\n")
+
+previous = 0
+run = True
+
+
+def print_math_results():
+    print(add(7, 5))
+    print(subtract(7, 5))
+    print(multiply(7, 5))
+    print(divide(7, 5))
+
+
+def perform_math():
+    global run
+    global previous
+
+    if previous == 0:
+        equation = input("Enter an equation: ")
+    else:
+        equation = input(str(previous))
+
+    if equation == 'quit':
+        run = False
+    else:
+        equation = re.sub('[a-zA-Z,.:()" "]', '', equation)
+        """print_math_results()"""
+
+        if previous == 0:
+            previous = eval(equation)
+        else:
+            previous = eval(str(previous) + equation)
+
+
+while run:
+    perform_math()
