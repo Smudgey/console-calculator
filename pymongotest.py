76898f1ab67b126d6f04fbb1931115a37553663b
Luke Smith <lukesmudgesmith@gmail.com>
2017-01-10 08:51:32 +0000

    improved format of webscraper and added inital pymongo file

---
+++ pymongotest.py
@@ -0,0 +1,4 @@
+from pymongo import MongoClient
+
+
+myClient = MongoClient()
--- webscrape.py
+++ webscrape.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 from PIL import Image
 from io import BytesIO
+import os
 
 search = input("Enter 'search' or 'images' ")
 
@@ -25,14 +26,25 @@ if search == 'search':
 elif search == 'images':
     search = input("Search for: ")
     params = {"q": search}
+    dir_name = search.replace(" ", "_").lower()
+
+    if not os.path.isdir("./image_searches/" + dir_name):
+        os.makedirs("./image_searches/" + dir_name)
+
     r = requests.get("http://www.bing.com/images/search", params=params)
 
     soup = BeautifulSoup(r.text, "html.parser")
     links = soup.findAll("a", {"class": "thumb"})
 
     for item in links:
-        img_obj = requests.get(item.attrs["href"])
-        print("Getting:", item.attrs["href"])
-        title = item.attrs["href"].split("/")[-1]
-        img = Image.open(BytesIO(img_obj.content))
-        img.save('./image_searches/' + search + '/' + title, img.format)
+        try:
+            img_obj = requests.get(item.attrs["href"])
+            print("Getting:", item.attrs["href"])
+            title = item.attrs["href"].split("/")[-1]
+            try:
+                img = Image.open(BytesIO(img_obj.content))
+                img.save('./' + dir_name + '/' + title, img.format)
+            except:
+                print("Could not save image")
+        except:
+            print("Could not load image")
